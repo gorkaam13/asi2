7004adf2aadc0f845a7b55a99ec12af079552de9
AS3-2-01 <AS3-2-01@uni.lan>
2019-10-29 09:19:42 +0100

    29/10/2019

---
+++ function.php
@@ -0,0 +1,18 @@
+<?php
+
+function echo_izena($s="kaixo", $s2="mundua!") {
+    echo "$s $s2 <br>";
+}
+function echo_izena_3() {
+    for ($i=0; $i<3; $i++) {
+        echo_izena("Hello", "World!");
+    }
+}
+/*
+for ($i=0; $i<3; $i++) {
+    echo_izena();
+}
+*/
+echo_izena();
+echo "--------<br>";
+echo_izena_3();
---
+++ oop_example.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'simpleclass.php';
+$s = new SimpleClass();
+$s->displayvar();
+$s->var = 'Hello!!';
+$s->displayvar();
+
+//var_dump($s);
+?>
