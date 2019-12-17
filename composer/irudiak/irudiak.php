ceaf15b82905d890560093d49a34a569b2ae048a
php2 <gorkaam13@gmail.com>
2019-12-17 08:41:35 +0000

    17/12/2019

--- composer/irudiak/irudiak.php
+++ composer/irudiak/irudiak.php
@@ -8,31 +8,7 @@ use Intervention\Image\ImageManagerStatic as Image;
 // configure with favored image driver (gd by default)
 Image::configure(array('driver' => 'imagick'));
 // and you are ready to go ...
-$image = Image::make('../img/image.jpg')->resize(300, 200);
+$image = Image::make('image.jpg')->resize(300, 200);
 //// finally we save the image as a new file
 //$image->save('eibar2.jpg');
 //
-
-$image->encode('png');
-$type = 'png';
-$base64 = 'data:image/' . $type . ';base64,' . base64_encode($image);
-echo "<img src=$base64>";
-//*********** 
-// resize the image to a width of 300 and constrain aspect ratio (auto height)
-$image = Image::make('../img/image.jpg');
-$image->resize(300, null, function ($constraint) {
-    $constraint->aspectRatio();
-});
-$image->greyscale();
-$image->encode('png');
-$type = 'png';
-$base64 = 'data:image/' . $type . ';base64,' . base64_encode($image);
-echo "<img src=$base64>";
-//*********** */
-$image = Image::make('../img/image.jpg');
-// rotate image 45 degrees clockwise
-$image->rotate(-45);
-$image->encode('png');
-$type = 'png';
-$base64 = 'data:image/' . $type . ';base64,' . base64_encode($image);
-echo "<img src=$base64>";
