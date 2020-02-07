192f9b88570c18f076a675341a5cf9a716ae9e5f
php2 <gorkaam13@gmail.com>
2020-02-07 07:06:26 +0000

    07/02/2020

--- composer/tiempo/sql.sql
+++ composer/tiempo/sql.sql
@@ -9,10 +9,12 @@ create table users (
 	user varchar (30) not null,
 	pass varchar (250) not null,
 	PRIMARY KEY (user)
-	
 );
+
 INSERT INTO capitales (capital,latitud,longitud) VALUES 
  ('madrid','40.416','-3.702'),
  ('paris','48.853','2.348'),
  ('londres','51.507', '-0.127'),
 ('berlin','52.512', '13.326');
+INSERT INTO users  VALUES 
+ ('admin','admin');
